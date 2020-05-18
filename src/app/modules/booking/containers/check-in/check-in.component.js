10233f5a096a91f2166e42550e77daca03cfa6a2
Tam <tamvq@vng.com.vn>
2020-05-18 14:57:05 +0700

    feat: add birthday form to check-in page

--- src/app/modules/booking/containers/check-in/check-in.component.js
+++ src/app/modules/booking/containers/check-in/check-in.component.js
@@ -2,8 +2,9 @@
     const module = angular.module('module.booking.containers.check-in', []);
 
     class CheckInComponent {
-        constructor($scope, $sce, $state, BookingService) {
+        constructor($scope, $timeout, $sce, $state, BookingService) {
             this.$scope = $scope;
+            this.$timeout = $timeout;
             this.$sce = $sce;
             this.$state = $state;
             this.BookingService = BookingService;
@@ -15,16 +16,41 @@
             this.form = {
                 info: {
                     name: '',
-                    phone: ''
+                    phone: '',
+                    birthday: ''
                 }
             };
+
+            this.step = 1;
         }
+
+        checkin() {
+            this.BookingService.checkin(this.form.info).then(res => {
+                if (res && res[0].birthday) {
+                    alert('Check-in successfully!');
+
+                    this.$state.reload();
+                } else {
+                    this.$timeout(() => {
+                        this.step += 1;
+                    });
+                }
+            });
+        };
 
         done() {
             this.BookingService.checkin(this.form.info).then(res => {
-                alert('Check-in successfully!');
-            }).finally(() => this.$state.reload());
-        };
+                let message = 'Check-in successfully!';
+
+                if (this.form.info.birthday.date() === moment().date() && this.form.info.birthday.month() === moment().month()) {
+                    message = 'Happy birthday! Check-in successfully!';
+                }
+
+                alert(message);
+
+                this.$state.reload();
+            });
+        }
     }
 
     module.component('checkIn', {
